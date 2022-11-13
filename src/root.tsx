f0184fb4916912281bf04c525baeb88186bb3a68
gringofe <PGFvy85nC@protonmail.com>
2022-11-13 09:52:07 +0100

    Init

---
+++ src/root.tsx
@@ -0,0 +1,45 @@
+// @refresh reload
+import {ColorModeScript, HopeProvider, injectCriticalStyle} from "@hope-ui/core";
+import {Suspense} from "solid-js";
+import {
+    A,
+    Body,
+    ErrorBoundary,
+    FileRoutes,
+    Head,
+    Html,
+    Meta,
+    Routes,
+    Scripts,
+    Title,
+} from "solid-start";
+import "./root.css";
+
+export default function Root() {
+    injectCriticalStyle();
+
+    return (
+        <Html lang="en">
+            <Head>
+                <Title>SolidStart - Bare</Title>
+                <Meta charset="utf-8"/>
+                <Meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
+            <Body>
+                <ColorModeScript/>
+                <HopeProvider>
+                    <Suspense>
+                        <ErrorBoundary>
+                            <A href="/">Index</A>
+                            <A href="/about">About</A>
+                            <Routes>
+                                <FileRoutes/>
+                            </Routes>
+                        </ErrorBoundary>
+                    </Suspense>
+                </HopeProvider>
+                <Scripts/>
+            </Body>
+        </Html>
+    );
+}
